8e036344032bb21015a1361a68c1c95df98fad53
Jason Estep <jasonestep2001@yahoo.com>
2025-04-06 12:43:20 -0700

    Base site

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! Bev will be in touch soon.');
+  });
+  
